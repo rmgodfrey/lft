5d39c8981d923fe54f2b3708921be2f8bd31ba9a
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-05-30 08:40:23 -0500

    pull changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-s# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@ s# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_122633) do
+ActiveRecord::Schema.define(version: 2022_05_30_130558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_122633) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
