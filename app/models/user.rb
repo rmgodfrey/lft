837d69482cfb400162231367b998b4f30dd8d9f6
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-06-01 12:14:22 +0200

    Uploaded images seem to be saving correctly to cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :offers
-  # validates :location, presence: true
-  # validates :name, presence: true
+  has_one_attached :photo
+  validates :location, presence: true
+  validates :name, presence: true
 end
