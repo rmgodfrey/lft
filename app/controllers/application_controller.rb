9a2265168959ede1aaf18b5efb40320f1bb125d0
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-06-02 16:38:34 +0200

    work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :set_time_zone, if: :current_user
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :photo, :time_zone])
+  end
+
+  def set_time_zone(&block)
+    Time.zone = current_user.time_zone, &block
   end
 end
