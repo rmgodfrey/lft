3569831d93133be5db310ad7737148cedfeca0d8
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-06-02 18:51:18 +0200

    saving latest changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  around_action :set_time_zone, if: :current_user
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :photo, :time_zone])
-  end
-
-  def set_time_zone(&block)
-    Time.zone = current_user.time_zone, &block
   end
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,26 @@
 class BookingsController < ApplicationController
 
   def index
+    # Time.zone = current_user.time_zone
     if params[:time] == "future"
-      date_range = Time.zone.now..
+      date_range = Time.zone.now...
     else
       date_range = ...Time.zone.now
     end
     @bookings = Booking.where(user: current_user, starting_date: date_range)
-    raise
   end
 
   def new
+    Time.zone = current_user.time_zone
     @booking = Booking.new
   end
 
   def create
+    raise
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.offer = Offer.find(params[:offer_id])
+    raise
     @booking.save!
     redirect_to bookings_path("future")
   end
