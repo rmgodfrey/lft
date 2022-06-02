5ab474e5859ad31b0fb50d14fd53dae3e9dca0f5
Chris Wall <cbwall24@gmail.com>
2022-06-02 18:27:45 +0300

    tried to add photos to seeded users, still a work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+require "open-uri"
 # Create fake offers
 
 SUBJECTS = %w[
@@ -18,6 +11,8 @@ SUBJECTS = %w[
     name: Faker::Name.name, location: Faker::Address.city,
     email: Faker::Internet.email, password: Faker::Internet.password
   )
+  user.photo.attach(io: URI.open('app/assets/images/avatarexample.jpg'),
+                    filename: 'avatarexample.jpg')
   rand(4).times do
     offer = Offer.new(
       topic: SUBJECTS.sample, description: Faker::Lorem.paragraph,
